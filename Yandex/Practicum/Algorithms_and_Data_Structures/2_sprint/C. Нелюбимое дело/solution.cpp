c0d9666b9b7f5aa906218cbafc4e005f0e7249c6
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-09-12 20:47:52 +0300

    Yandex Solutions (2 sprint)

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/C. Нелюбимое дело/solution.cpp
@@ -0,0 +1,57 @@
+#ifdef REMOTE_JUDGE
+#include "solution.h"
+#endif
+
+#ifndef REMOTE_JUDGE
+#include <cassert>
+#include <string>
+
+struct Node {
+    std::string value;
+    Node* next;
+    Node(const std::string& value, Node* next) : value(value), next(next) {
+
+    }
+};
+#endif
+
+
+Node* solution(Node* head, int idx) {
+    /*
+        Time Complexity: O(N)
+        Memory Complexity: O(1)
+    */
+    if (!idx) {
+        return head->next;
+    }
+
+    Node* prev = head;
+    --idx;
+
+    while (idx > 0) {
+        prev = prev->next;
+        --idx;
+    }
+
+    prev->next = prev->next->next;
+    return head;
+}
+
+#ifndef REMOTE_JUDGE
+void test() {
+    Node node3("node3", nullptr);
+    Node node2("node2", &node3);
+    Node node1("node1", &node2);
+    Node node0("node0", &node1);
+    Node* new_head = solution(&node0, 1);
+    assert(new_head == &node0);
+    assert(new_head->next == &node2);
+    assert(new_head->next->next == &node3);
+    assert(new_head->next->next->next == nullptr);
+    // result is : node0 -> node2 -> node3
+}
+
+int main() {
+    test();
+}
+#endif
---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/D. Заботливая мама/solution.cpp
@@ -0,0 +1,51 @@
+#ifdef REMOTE_JUDGE
+#include "solution.h"
+#endif
+
+#ifndef REMOTE_JUDGE
+#include <cassert>
+#include <string>
+
+struct Node {
+    std::string value;
+    Node* next;
+    Node(const std::string& value, Node* next) : value(value), next(next) {
+
+    }
+};
+#endif
+
+
+int solution(Node* head, const std::string& elem) {
+    /*
+        Time Complexity: O(L) L - street length
+        Memory Complexity: O(1)
+    */
+    int idx = 0;
+
+    while (head) {
+        if (head->value == elem) {
+            break;
+        }
+
+        head = head->next;
+        ++idx;
+    }
+
+    return head != nullptr ? idx : -1;
+}
+
+#ifndef REMOTE_JUDGE
+void test() {
+    Node node3("node3", nullptr);
+    Node node2("node2", &node3);
+    Node node1("node1", &node2);
+    Node node0("node0", &node1);
+    int idx = solution(&node0, "node2");
+    assert(idx == 2);
+}
+
+int main() {
+    test();
+}
+#endif
