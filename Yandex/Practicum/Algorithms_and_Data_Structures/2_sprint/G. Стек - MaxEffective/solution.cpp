a5c0007b99d4acc057ed5997481f1a6d0ce7965f
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-09-16 14:21:55 +0300

    Added Solution "G. Стек - MaxEffective"

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/G. Стек - MaxEffective/solution.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class StackMax {
+public:
+	explicit StackMax(size_t size) {
+		stack_.reserve(size);
+		max_nums_.reserve(size);
+	}
+
+	void push(int x) {
+		stack_.push_back(x);
+
+		if (max_nums_.empty()) {
+			max_nums_.push_back(x);
+			return;
+		}
+
+		max_nums_.push_back(std::max(max_nums_.back(), x));
+	}
+
+	void pop() {
+		if (IsEmpty()) {
+			PrintEmptyStackError();
+			return;
+		}
+
+		stack_.pop_back();
+		max_nums_.pop_back();
+	}
+
+	void get_max() const {
+		if (IsEmpty()) {
+			PrintNone();
+			return;
+		}
+
+		std::cout << max_nums_.back() << '\n';
+	}
+
+	void top() const {
+		if (IsEmpty()) {
+			PrintEmptyStackError();
+			return;
+		}
+
+		std::cout << stack_.back() << '\n';
+	}
+
+private:
+	std::vector<int> stack_;
+	std::vector<int> max_nums_;
+
+	bool IsEmpty() const noexcept {
+		return stack_.empty();
+	}
+
+	static void PrintEmptyStackError() {
+		std::cout << "error" << '\n';
+	}
+
+	static void PrintNone() {
+		std::cout << "None" << '\n';
+	}
+};
+
+void Solution(size_t size) {
+	/*
+		Time Complexity: O(N)
+		Memory Complexity: O(N)
+	*/
+	StackMax stack(size);
+	size_t start = 0;
+
+	while (start < size) {
+		std::string command;
+		std::getline(std::cin >> std::ws, command);
+
+		if (command == "get_max") {
+			stack.get_max();
+		} else if (command == "pop") {
+			stack.pop();
+		} else if (command == "top") {
+			stack.top();
+		} else {
+			size_t space = command.find(' ');
+			int num = std::stoi(command.substr(space + 1));
+
+			stack.push(num);
+		}
+
+		++start;
+	}
+}
+
+int main() {
+	size_t count_commands;
+	std::cin >> count_commands;
+
+	Solution(count_commands);
+}
