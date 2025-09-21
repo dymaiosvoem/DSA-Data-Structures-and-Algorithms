d39087f571ebfd4200cc1b4c048ab59f3325f611
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-09-21 16:29:10 +0300

    Improved Solution from O(2^N) to O(N)

--- Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/K. Рекурсивные числа Фибоначчи/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/K. Рекурсивные числа Фибоначчи/solution.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
+#include <unordered_map>
 
-size_t Solution(size_t num_commits) {
-	if (num_commits == 1 || num_commits == 0) {
-		return 1;
+size_t Solution(size_t intern_number, std::unordered_map<size_t, size_t>& memo) {
+	/*
+		Time Complexity: T(N) N - count recursion
+		Memory Complexity: O(M) M - memo size
+	*/
+	auto it = memo.find(intern_number);
+
+	if (it != memo.end()) {
+		return it->second;
 	}
 
-	return Solution(num_commits - 1) + Solution(num_commits - 2);
+	memo[intern_number] = Solution(intern_number - 1, memo) + Solution(intern_number - 2, memo);
+
+	return memo[intern_number];
 }
 
 int main() {
-	size_t num_commits;
-	std::cin >> num_commits;
+	size_t intern_number;
+	std::cin >> intern_number;
 
-	std::cout << Solution(num_commits) << '\n';
+	std::unordered_map<size_t, size_t> memo = { { 0, 1 }, {1, 1} };
+	memo.reserve(intern_number + 1);
+
+	size_t result = Solution(intern_number, memo);
+
+	std::cout << result << '\n';
 }
