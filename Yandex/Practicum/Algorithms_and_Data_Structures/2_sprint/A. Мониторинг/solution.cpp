f9994e86d6ba61a5926f768e39b127fe49865f03
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-09-21 15:27:48 +0300

    Refactoring and improved output

--- Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/A. Мониторинг/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/2_sprint/A. Мониторинг/solution.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 #include <vector>
 
-void Solution(std::vector<std::vector<int>>& mtrx) {
-
-}
-
-int main() {
-	size_t row, column;
-	std::cin >> row >> column;
-
-	size_t mtrx_size = column * row;
-
-	std::vector<int> matrix(mtrx_size, 0);
-
+void Solution(std::vector<int>& matrix, size_t row, size_t column) {
+	/*
+		Time Complexity: O(R * C)
+		Memory Comlexity: O(N) N - size matrix
+	*/
 	size_t start = 0;
 
 	for (size_t i = 0; i < row; ++i) {
@@ -27,24 +20,23 @@ int main() {
 		start = 0;
 	}
 
-	bool space = true;
-
-	for (size_t i = 0; i < mtrx_size; ++i) {
-		if (space) {
-			std::cout << matrix[i];
-			space = false;
-			continue;
-		} else if (row == 1) {
-			std::cout << '\n';
-			std::cout << matrix[i];
-			continue;
+	for (size_t i = 0; i < column; ++i) {
+		for (size_t j = 0; j < row; ++j) {
+			if (j) {
+				std::cout << ' ';
+			}
+			std::cout << matrix[i * row + j];
 		}
-
-		std::cout << ' ' << matrix[i];
-
-		if (i != 0 && (i + 1) % row == 0) {
-			std::cout << '\n';
-			space = true;
-		}
+		std::cout << '\n';
 	}
+}
+
+int main() {
+	size_t row, column;
+	std::cin >> row >> column;
+
+	size_t mtrx_size = column * row;
+	std::vector<int> matrix(mtrx_size, 0);
+
+	Solution(matrix, row, column);
 }
