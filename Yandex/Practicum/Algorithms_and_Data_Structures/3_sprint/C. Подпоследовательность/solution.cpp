ccb2e821eb48077e5ce59ac8b254b6dae55598ce
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-10-10 15:26:16 +0300

    Added Solution "C. Подпоследовательность"

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/3_sprint/C. Подпоследовательность/solution.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+void Solution(std::string_view s, std::string_view t) {
+    size_t i = 0, j = 0;
+
+    while (i < s.size() && j < t.size()) {
+        if (s[i] == t[j]) {
+            ++i;
+        }
+        ++j;
+    }
+
+    std::cout << (i == s.size() ? "True" : "False") << std::endl;
+}
+
+int main() {
+    std::string s, t;
+    std::cin >> s >> t;
+
+    Solution(s, t);
+}
