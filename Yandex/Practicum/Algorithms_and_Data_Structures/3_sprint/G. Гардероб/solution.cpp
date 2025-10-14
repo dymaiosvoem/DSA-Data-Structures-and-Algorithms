934101c6f9fecec35b7f82a44f3bddc917eadac9
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-10-14 13:01:21 +0300

    Improved The Solution And Evaluation Algorithms

--- Yandex/Practicum/Algorithms_and_Data_Structures/3_sprint/G. Гардероб/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/3_sprint/G. Гардероб/solution.cpp
@@ -1,26 +1,28 @@
 ﻿#include <iostream>
 #include <vector>
 
-void Solution(const std::vector<size_t>& colors_for_items, size_t count_items_in_wardrobe,
-	size_t count_colors) {
+void Solution(const std::vector<size_t>& colors_for_items) {
 	/*
-		Time Complexity: O(N) N - count_items_in_wardrobe
-		Memory Complexity: O(C) C - count_colors
+		Time Complexity: O(N) N - colors_for_items.size()
+		Memory Complexity: O(1) count colors - 3
 	*/
-	std::vector<size_t> result(count_colors);
+	std::vector<size_t> result = { 0, 0, 0 };
 
-	for (size_t i = 0; i < count_items_in_wardrobe; ++i) {
-		size_t value = colors_for_items[i];
-		++result[value];
+	for (size_t i = 0; i < colors_for_items.size(); ++i) {
+		++result[colors_for_items[i]];
 	}
 
-	size_t value_color = 0;
+	bool printed_anything = false;
 
 	for (size_t i = 0; i < result.size(); ++i) {
 		for (size_t j = 0; j < result[i]; ++j) {
-			std::cout << value_color << ' ';
+			if (!printed_anything) {
+				std::cout << i;
+				printed_anything = true;
+				continue;
+			}
+			std::cout << ' ' << i;
 		}
-		++value_color;
 	}
 }
 
@@ -28,12 +30,10 @@ int main() {
 	size_t count_items_in_wardrobe;
 	std::cin >> count_items_in_wardrobe;
 
-	size_t count_colors = 3;
-
 	std::vector<size_t> colors_for_items(count_items_in_wardrobe);
 	for (size_t i = 0; i < colors_for_items.size(); ++i) {
 		std::cin >> colors_for_items[i];
 	}
 
-	Solution(colors_for_items, count_items_in_wardrobe, count_colors);
+	Solution(colors_for_items);
 }
