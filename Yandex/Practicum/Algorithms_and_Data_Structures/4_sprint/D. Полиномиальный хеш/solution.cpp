3363866ec849c627439b3c5ead3d62e2585754e5
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-11-13 13:36:46 +0300

    Added Solution "D. Полиномиальный хеш"

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/4_sprint/D. Полиномиальный хеш/solution.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+void Solution(size_t base, size_t module, const std::string& s) {
+	/*
+		Time Complexity: O(N)
+		Memory Complexity: O(1)
+	*/
+	size_t hash = 0;
+
+	for (char ch : s) {
+		hash = (hash * base + static_cast<size_t>(ch)) % module;
+	}
+
+	std::cout << hash << '\n';
+}
+
+int main() {
+	size_t base, module;
+	std::cin >> base >> module;
+
+	std::string s;
+	std::cin >> s;
+
+	Solution(base, module, s);
+}
