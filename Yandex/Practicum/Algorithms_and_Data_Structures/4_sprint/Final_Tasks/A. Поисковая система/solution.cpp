2ca64db539cdbcca2d60c785e37c4beb428dc30e
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-11-25 17:39:25 +0300

    Refactored The Solution And Added Complexity Score

--- Yandex/Practicum/Algorithms_and_Data_Structures/4_sprint/Final_Tasks/A. Поисковая система/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/4_sprint/Final_Tasks/A. Поисковая система/solution.cpp
@@ -1,136 +1,171 @@
 ﻿#include <algorithm>
 #include <iostream>
-#include <istream>
 #include <string>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
-
-void Solution() {
-	/*
-		Time Complexity:
-		Memory Complexity:
-	*/
-
-}
 
 struct IdAndFreq {
 	size_t doc_id = 0;
 	size_t freq = 0;
 };
 
-int main() {
-	size_t n;
-	std::cin >> n;
+std::vector<std::string> SplitBySpace(const std::string& str) {
+	std::vector<std::string> words;
+	std::string word;
 
-	std::unordered_map<std::string, std::vector<IdAndFreq>> word_id_freq;
+	for (char w : str) {
+		if (w == ' ') {
+			if (!word.empty()) {
+				words.push_back(word);
+				word.clear();
+			}
+		} else {
+			word += w;
+		}
+	}
 
+	if (!word.empty()) {
+		words.push_back(word);
+	}
+
+	return words;
+}
+
+void ParsingDocuments(std::unordered_map<std::string, std::vector<IdAndFreq>>& word_id_freq, size_t n) {
 	for (size_t i = 0; i < n; ++i) {
 		std::string doc;
 		std::getline(std::cin >> std::ws, doc);
 
 		std::unordered_map<std::string, size_t> word_freq;
 
-		std::string word;
-		size_t start = 0;
+		std::vector<std::string> words = SplitBySpace(doc);
+		word_freq.reserve(words.size());
 
-		while (start < doc.size()) {
-			if (doc[start] == ' ') {
-				if (!word.empty()) {
-					word_freq[word] += 1;
-					word.clear();
-				}
-			} else {
-				word += doc[start];
-			}
-
-			++start;
-		}
-
-		if (!word.empty()) {
-			word_freq[word] += 1;
+		for (const auto& word : words) {
+			++word_freq[word];
 		}
 
 		for (const auto& [key, value] : word_freq) {
 			word_id_freq[key].emplace_back(IdAndFreq{ i + 1, value });
 		}
 	}
+}
+
+std::unordered_set<std::string> ParsingQuery() {
+	std::string query;
+	std::getline(std::cin >> std::ws, query);
+
+	std::unordered_set<std::string> uniq_words;
+
+	std::vector<std::string> words = SplitBySpace(query);
+	uniq_words.reserve(words.size());
+
+	for (const auto& word : words) {
+		uniq_words.emplace(word);
+	}
+
+	return uniq_words;
+}
+
+std::vector<std::pair<size_t, size_t>> ProcessQuery(const std::unordered_map<std::string, std::vector<IdAndFreq>>& word_id_freq) {
+	std::unordered_set<std::string> uniq_words = ParsingQuery();
+
+	std::unordered_map<size_t, size_t> id_relevance;
+
+	for (const auto& word : uniq_words) {
+		auto it = word_id_freq.find(word);
+
+		if (it != word_id_freq.end()) {
+			for (const auto& [doc_id, freq] : it->second) {
+				id_relevance[doc_id] += freq;
+			}
+		}
+	}
+
+	std::vector<std::pair<size_t, size_t>> result;
+	result.reserve(id_relevance.size());
+
+	for (const auto& [doc_id, relevance] : id_relevance) {
+		result.emplace_back(doc_id, relevance);
+	}
+
+	std::sort(result.begin(), result.end(), [](const auto& left, const auto& right) {
+		if (left.second == right.second) {
+			return left.first < right.first;
+		}
+
+		return left.second > right.second;
+		});
+
+	return result;
+}
+
+void PrintResult(const std::vector<std::pair<size_t, size_t>>& result) {
+	size_t printed = 0;
+	bool space = false;
+
+	for (const auto& [key, value] : result) {
+		if (printed == 5) {
+			break;
+		}
+
+		if (space) {
+			std::cout << ' ' << key;
+		} else {
+			std::cout << key;
+			space = true;
+		}
+
+		++printed;
+	}
+
+	std::cout << '\n';
+}
+
+void Solution() {
+	/*
+		Time Complexity:   ParsingDocuments - O(L_d), L_d - length_documents
+
+						   ProcessQuery - O(L_q) + O(P) + O(R * log R),
+						   L_q - length_queries,
+						   P - количество (слово, документ) - пар, по которым прошелся
+						   R - количество документов с ненулевой релевантностью, size_of(result)
+
+						   PrintResult - O(1), печатаю не более 5 документов
+
+						   Result = O(L_q) + O(P) + O(R * log R);
+
+		Memory Complexity: ParsingDocuments: O(U_docs) + O(P_docs) = O(P_docs),
+						   U_docs - количество уникальных слов во всех документах
+						   P_docs - количество всех пар (слово, IdAndFreq)
+
+						   ProcessQuery - O(U_q) + O(I) + O(S),
+						   U_q - количество уникальных слов запроса;
+						   I - количество пар (id, relevance);
+						   S - количество отсортированных пар (id, relevance)
+
+						   PrintResult - O(1)
+
+						   Result = O(U_q) + O(I) + O(S);
+	*/
+	size_t n;
+	std::cin >> n;
+
+	std::unordered_map<std::string, std::vector<IdAndFreq>> word_id_freq;
+
+	ParsingDocuments(word_id_freq, n);
 
 	size_t m;
 	std::cin >> m;
 
 	for (size_t j = 0; j < m; ++j) {
-		std::string request;
-		std::getline(std::cin >> std::ws, request);
+		auto result = ProcessQuery(word_id_freq);
 
-		std::unordered_set<std::string> uniq_words;
-		uniq_words.reserve(request.size());
-
-		std::string word;
-		size_t start = 0;
-
-		while (start < request.size()) {
-			if (request[start] == ' ') {
-				if (!word.empty()) {
-					uniq_words.emplace(word);
-					word.clear();
-				}
-			} else {
-				word += request[start];
-			}
-
-			++start;
-		}
-
-		if (!word.empty()) {
-			uniq_words.emplace(word);
-		}
-
-		std::unordered_map<size_t, size_t> id_relevance;
-
-		for (const auto& word : uniq_words) {
-			auto it = word_id_freq.find(word);
-
-			if (it != word_id_freq.end()) {
-				for (const auto& [doc_id, freq] : it->second) {
-					id_relevance[doc_id] += freq;
-				}
-			}
-		}
-
-		std::vector<std::pair<size_t, size_t>> result;
-		result.reserve(id_relevance.size());
-
-		for (const auto& [doc_id, relevance] : id_relevance) {
-			result.emplace_back(static_cast<int>(doc_id), relevance);
-		}
-
-		std::sort(result.begin(), result.end(), [&](const auto& left, const auto& right) {
-			if (left.second == right.second) {
-				return left.first < right.first;
-			}
-
-			return left.second > right.second;
-			});
-
-		size_t printed = 0;
-		bool space = false;
-
-		for (const auto& [key, value] : result) {
-			if (printed == 5) {
-				break;
-			}
-
-			if (space) {
-				std::cout << ' ' << key;
-			} else {
-				std::cout << key;
-				space = true;
-			}
-
-			++printed;
-		}
-
-		std::cout << '\n';
+		PrintResult(result);
 	}
+}
+
+int main() {
+	Solution();
 }
