c266a53ba494836dad8c074ced96e888f190e2b4
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-12-11 11:52:32 +0300

    Added Solution "I. Разные деревья поиска"

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/5_sprint/I. Разные деревья поиска/solution.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+void Solution(size_t n) {
+	/*
+		Time Complexity: O(N^2)
+		Memory Complexity: O(N)
+	*/
+	std::vector<size_t> T(n + 1);
+	T[0] = 1;
+	T[1] = 1;
+
+	for (size_t k = 2; k <= n; ++k) {
+		T[k] = 0;
+
+		for (size_t i = 1; i <= k; ++i) {
+			T[k] += T[i - 1] * T[k - i];
+		}
+	}
+
+	std::cout << T[n] << '\n';
+}
+
+int main() {
+	size_t n;
+	std::cin >> n;
+
+	Solution(n);
+}
