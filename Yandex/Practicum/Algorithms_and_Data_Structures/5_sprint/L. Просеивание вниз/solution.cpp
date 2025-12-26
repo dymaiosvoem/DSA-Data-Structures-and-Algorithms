41469a27a225d45c724602cee887b2002167d596
dymaiosvoem <dymaiosvoem@yandex.ru>
2025-12-26 19:33:30 +0300

    Refactor: make SiftDown iterative (O(1) extra memory)

--- Yandex/Practicum/Algorithms_and_Data_Structures/5_sprint/L. Просеивание вниз/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/5_sprint/L. Просеивание вниз/solution.cpp
@@ -14,29 +14,37 @@ int MaxChildIndex(const std::vector<int>& heap, int idx_left_child, int idx_righ
 }
 
 int siftDown(std::vector<int>& heap, int idx) {
-    int idx_left_child = 2 * idx;
+    /*
+        Time Complexity: O(log N)
+        Memory Complexity: O(1)
+    */
+    while (true) {
+        int idx_left_child = 2 * idx;
 
-    if (idx_left_child >= heap.size()) {
-        return idx;
-    }
-
-    int idx_right_child = (2 * idx) + 1;
-
-    if (idx_right_child >= heap.size()) {
-        if (heap[idx] < heap[idx_left_child]) {
-            std::swap(heap[idx], heap[idx_left_child]);
-            idx = siftDown(heap, idx_left_child);
+        if (idx_left_child >= heap.size()) {
+            return idx;
         }
-    } else {
-        int best_child = MaxChildIndex(heap, idx_left_child, idx_right_child);
 
-        if (heap[idx] < heap[best_child]) {
-            std::swap(heap[idx], heap[best_child]);
-            idx = siftDown(heap, best_child);
+        int idx_right_child = (2 * idx) + 1;
+
+        if (idx_right_child >= heap.size()) {
+            if (heap[idx] < heap[idx_left_child]) {
+                std::swap(heap[idx], heap[idx_left_child]);
+                idx = idx_left_child;
+            } else {
+                return idx;
+            }
+        } else {
+            int best_child = MaxChildIndex(heap, idx_left_child, idx_right_child);
+
+            if (heap[idx] < heap[best_child]) {
+                std::swap(heap[idx], heap[best_child]);
+                idx = best_child;
+            } else {
+                return idx;
+            }
         }
     }
-
-    return idx;
 }
 
 #ifndef REMOTE_JUDGE
