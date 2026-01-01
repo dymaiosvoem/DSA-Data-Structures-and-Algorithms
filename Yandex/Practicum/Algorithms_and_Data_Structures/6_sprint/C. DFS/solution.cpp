1c4ec0b2720781e515da1d35307722606d95c182
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-01-01 16:59:20 +0300

    Added Solution "C. DFS"

--- Yandex/Practicum/Algorithms_and_Data_Structures/6_sprint/C. DFS/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/6_sprint/C. DFS/solution.cpp
@@ -1,1 +1,96 @@
-﻿
+﻿#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+enum class Color {
+	WHITE,
+	GREY,
+	BLACK
+};
+
+struct VertexInfo {
+	VertexInfo(size_t vertex, bool exiting) : v_(vertex), exiting_(exiting) {}
+
+	size_t v_ = 0;
+	bool exiting_ = false;
+};
+
+std::vector<size_t> DFS(std::vector<std::vector<size_t>>& adjacency_list, size_t starting_vertex) {
+	std::vector<size_t> result;
+	result.reserve(adjacency_list.size());
+
+	std::vector<Color> color(adjacency_list.size(), Color::WHITE);
+
+	std::stack<VertexInfo> stack;
+	color[starting_vertex] = Color::GREY;
+	stack.push(VertexInfo(starting_vertex, false));
+
+	while (!stack.empty()) {
+		auto current = stack.top();
+		stack.pop();
+
+		if (current.exiting_) {
+			color[current.v_] = Color::BLACK;
+			continue;
+		}
+
+		result.push_back(current.v_);
+		stack.push(VertexInfo(current.v_, true));
+
+		for (auto& w : adjacency_list[current.v_]) {
+			if (color[w] == Color::WHITE) {
+				color[w] = Color::GREY;
+				stack.push(VertexInfo(w, false));
+			}
+		}
+	}
+
+	return result;
+}
+
+void PrintResult(std::vector<size_t>& result) {
+	for (size_t i = 0; i < result.size(); ++i) {
+		if (i) {
+			std::cout << ' ' << result[i];
+		} else {
+			std::cout << result[i];
+		}
+	}
+}
+
+void Solution(size_t vertices, size_t edges) {
+	/*
+		Time Complexity:
+		Memory Complexity:
+	*/
+	std::vector<std::vector<size_t>> adjacency_list(vertices + 1, std::vector<size_t>());
+
+	for (size_t edge = 0; edge < edges; ++edge) {
+		size_t u, v;
+		std::cin >> u >> v;
+
+		adjacency_list[u].push_back(v);
+		adjacency_list[v].push_back(u);
+	}
+
+	for (size_t idx = 1; idx < adjacency_list.size(); ++idx) {
+		std::sort(adjacency_list[idx].begin(), adjacency_list[idx].end(), [](const size_t lhs, const size_t rhs) {
+			return lhs > rhs;
+			});
+	}
+
+	size_t starting_vertex;
+	std::cin >> starting_vertex;
+
+	auto result = DFS(adjacency_list, starting_vertex);
+
+	PrintResult(result);
+}
+
+int main() {
+	size_t n, m;
+	std::cin >> n >> m;
+
+	Solution(n, m);
+}
