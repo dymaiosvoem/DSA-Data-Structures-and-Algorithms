e8fe09212dd6e0c341ca37f72e52bb821a69a9ff
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-01-27 19:48:47 +0300

    added "A. Расстояние по Левенштейну"

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/7_sprint/Final_Tasks/A. Расстояние по Левенштейну/solution.cpp
@@ -0,0 +1,42 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+size_t ComputeLevenshteinDistance(std::string_view s, std::string_view t) {
+	size_t s_size = s.size(), t_size = t.size();
+
+	if (s_size == 0 || t_size == 0) {
+		return std::max(s_size, t_size);
+	}
+
+	std::vector<std::vector<size_t>> dp(s_size + 1, std::vector<size_t>(t_size + 1, 0));
+
+	for (size_t i = 0; i <= s_size; ++i) {
+		for (size_t j = 0; j <= t_size; ++j) {
+			if (i == 0) {
+
+			}
+			if (s[i - 1] == t[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1];
+			} else {
+				size_t symbol_replacement = dp[i - 1][j - 1] + 1;
+				size_t symbol_inserting = dp[i][j - 1] + 1;
+				size_t symbol_deleting = dp[i - 1][j] + 1;
+
+				dp[i][j] = std::max({ symbol_replacement , symbol_inserting, symbol_deleting });
+			}
+		}
+	}
+
+	return dp[s_size][t_size];
+}
+
+int main() {
+	std::string s, t;
+	std::cin >> s >> t;
+
+	auto distance = ComputeLevenshteinDistance(s, t);
+	std::cout << distance << '\n';
+}
