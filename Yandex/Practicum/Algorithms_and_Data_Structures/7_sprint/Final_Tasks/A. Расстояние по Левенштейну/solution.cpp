04d91b77e9e86c9f68570cf9f39534c1dfe9f171
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-01-28 15:08:31 +0300

    refactor: separate base case initialization for levenshtein DP

--- Yandex/Practicum/Algorithms_and_Data_Structures/7_sprint/Final_Tasks/A. Расстояние по Левенштейну/solution.cpp
+++ Yandex/Practicum/Algorithms_and_Data_Structures/7_sprint/Final_Tasks/A. Расстояние по Левенштейну/solution.cpp
@@ -1,36 +1,31 @@
 ﻿#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <string>
-#include <string_view>
 #include <vector>
 
-size_t ComputeLevenshteinDistance(std::string_view s, std::string_view t) {
+size_t ComputeLevenshteinDistance(const std::string& s, const std::string& t) {
 	size_t s_size = s.size(), t_size = t.size();
 
-	if (s_size == 0 || t_size == 0) {
-		return std::max(s_size, t_size);
+	std::vector<std::vector<size_t>> dp_dist(s_size + 1, std::vector<size_t>(t_size + 1, 0));
+
+	std::iota(dp_dist[0].begin(), dp_dist[0].end(), 0);
+
+	for (size_t k = 1; k <= s_size; ++k) {
+		dp_dist[k][0] = k;
 	}
 
-	std::vector<std::vector<size_t>> dp(s_size + 1, std::vector<size_t>(t_size + 1, 0));
-
-	for (size_t i = 0; i <= s_size; ++i) {
-		for (size_t j = 0; j <= t_size; ++j) {
-			if (i == 0) {
-
-			}
+	for (size_t i = 1; i <= s_size; ++i) {
+		for (size_t j = 1; j <= t_size; ++j) {
 			if (s[i - 1] == t[j - 1]) {
-				dp[i][j] = dp[i - 1][j - 1];
+				dp_dist[i][j] = dp_dist[i - 1][j - 1];
 			} else {
-				size_t symbol_replacement = dp[i - 1][j - 1] + 1;
-				size_t symbol_inserting = dp[i][j - 1] + 1;
-				size_t symbol_deleting = dp[i - 1][j] + 1;
-
-				dp[i][j] = std::max({ symbol_replacement , symbol_inserting, symbol_deleting });
+				dp_dist[i][j] = std::min({ dp_dist[i - 1][j] + 1, dp_dist[i][j - 1] + 1, dp_dist[i - 1][j - 1] + 1 });
 			}
 		}
 	}
 
-	return dp[s_size][t_size];
+	return dp_dist[s_size][t_size];
 }
 
 int main() {
