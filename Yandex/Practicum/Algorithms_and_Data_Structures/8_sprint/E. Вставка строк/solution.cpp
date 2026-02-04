681ac46aff5e4d0ad63ef480ef9f8c5e94d0c154
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-02-04 15:08:59 +0300

    added solution "E. Вставка строк"

---
+++ Yandex/Practicum/Algorithms_and_Data_Structures/8_sprint/E. Вставка строк/solution.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+void CombineAndPrintStrings(std::string& s, size_t gift_strings) {
+	/*
+		Time Complexity: O(N), N - pocket.size()
+		Memory Complexity: O(N)
+	*/
+	std::vector<std::string> mapping_ki_to_ti(s.size() + 1, "");
+
+	for (size_t i = 0; i < gift_strings; ++i) {
+		std::string t_i;
+		size_t k_i;
+
+		std::cin >> t_i >> k_i;
+
+		mapping_ki_to_ti[k_i] = t_i;
+	}
+
+	for (size_t j = 0; j < mapping_ki_to_ti.size(); ++j) {
+		if (mapping_ki_to_ti[j] != "") {
+			std::cout << mapping_ki_to_ti[j];
+		}
+
+		if (j < s.size()) {
+			std::cout << s[j];
+		}
+	}
+
+	std::cout << '\n';
+}
+
+int main() {
+	std::string str;
+	std::cin >> str;
+
+	size_t gift_strings;
+	std::cin >> gift_strings;
+
+	CombineAndPrintStrings(str, gift_strings);
+}
